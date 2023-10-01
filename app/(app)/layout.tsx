c9b8eec8e2f1311c0191797b169dec63da6e2a07
Lucas Borges Barbosa <lucasborgesbarbosa2012@gmail.com>
2023-10-01 01:35:00 -0300

    feat: create app layout using header/sidebar

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,36 @@
+"use client";
+import Header from "@/components/header/index";
+import Sidebar from "@/components/sidebar/index";
+import { useAuth } from "@/contexts/AuthContext";
+import { ReactNode, useState } from "react";
+
+interface AppLayoutProps {
+  children: ReactNode;
+}
+
+export default function AppLayout({ children }: AppLayoutProps) {
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const { user } = useAuth();
+  return (
+    <html className="h-full bg-gray-100">
+      <body className="h-full">
+        <div className="flex h-screen overflow-hidden">
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            <Header
+              sidebarOpen={sidebarOpen}
+              setSidebarOpen={setSidebarOpen}
+              userName={user?.name}
+              role={user?.role}
+            />
+            <main>
+              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                {children}
+              </div>
+            </main>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
